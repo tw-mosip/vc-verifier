202e2ef2a61d371dc2b2578aff16f52c9d922718
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2024-10-14 15:29:14 +0530

    [INJIMOB-1862] add vc verification logic for mso_mdoc VC
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/CredentialVerifierFactory.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/CredentialVerifierFactory.kt
@@ -1,14 +1,18 @@
 package io.mosip.vercred.vcverifier.credentialverifier
 
 import io.mosip.vercred.vcverifier.constants.CredentialFormat
+import io.mosip.vercred.vcverifier.constants.CredentialFormat.*
 import io.mosip.vercred.vcverifier.credentialverifier.types.LdpVcCredentialVerifier
+import io.mosip.vercred.vcverifier.credentialverifier.types.MsoMdocCredentialVerifier
 
 class CredentialVerifierFactory {
     fun verify(credential: String, credentialFormat: CredentialFormat): Boolean {
         return when (credentialFormat) {
-            CredentialFormat.LDP_VC -> LdpVcCredentialVerifier().verify(
+            LDP_VC -> LdpVcCredentialVerifier().verify(
                 credential = credential
             )
+
+            MSO_MDOC -> MsoMdocCredentialVerifier().verify(credential)
         }
     }
 }
--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/utils/Util.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/utils/Util.kt
@@ -1,6 +1,65 @@
 package io.mosip.vercred.vcverifier.utils
 
+import android.annotation.SuppressLint
+import android.os.Build
+import java.io.ByteArrayOutputStream
+import java.security.MessageDigest
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.time.format.DateTimeParseException
+import java.util.Base64
+
 class Util {
     val isAndroid: Boolean
         get() = System.getProperty("java.vm.name")?.contains("Dalvik") == true
+
+
+    @SuppressLint("NewApi")
+    fun decodeFromBase64UrlFormatEncoded(content: String): ByteArray {
+        return if (BuildConfig.getVersionSDKInt() >= Build.VERSION_CODES.O) {
+            Base64.getUrlDecoder().decode(content.toByteArray())
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+    }
+
+    @SuppressLint("NewApi")
+    fun decodeFromBase64FormatEncoded(content: String): ByteArray {
+        return if (BuildConfig.getVersionSDKInt() >= Build.VERSION_CODES.O) {
+            Base64.getDecoder().decode(content.toByteArray())
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+    }
+
+    @SuppressLint("NewApi")
+    fun isTimestamp(text: String?): Boolean {
+        if (BuildConfig.getVersionSDKInt() >= Build.VERSION_CODES.O) {
+            try {
+                // Try to parse as a timestamp (e.g., "2024-10-03T10:15:30")
+                Instant.parse(text)
+                return true
+            } catch (e: DateTimeParseException) {
+                println(e.message)
+                return false
+            }
+        } else return false
+    }
+
+    @SuppressLint("NewApi")
+    fun convertLocalDateTimeStringToInstant(dateStr: String): Instant {
+        return if (BuildConfig.getVersionSDKInt() >= Build.VERSION_CODES.O) {
+            val localDate = LocalDate.parse(dateStr)
+            localDate.atStartOfDay(ZoneOffset.UTC).toInstant()
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+    }
+
+     fun calculateDigest(
+         algorithm: String,
+         data: ByteArrayOutputStream,
+    ): ByteArray =
+        MessageDigest.getInstance(algorithm).digest(data.toByteArray())
 }
