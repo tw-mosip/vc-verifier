5c91d370d5a22cec1550c729c490034f10e41eec
BalachandarG <balachandar.g@thoughtworks.com>
2024-10-15 20:36:32 +0530

    [INJIMOB-1325]: Handling validation both Data model 1.1 and 2.0 and Updating Tests.
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>

--- vc-verifier/kotlin/vcverifier/src/test/java/io/mosip/vercred/vcverifier/UtilsTest.kt
+++ vc-verifier/kotlin/vcverifier/src/test/java/io/mosip/vercred/vcverifier/UtilsTest.kt
@@ -1,5 +1,6 @@
 package io.mosip.vercred.vcverifier
 
+import io.mosip.vercred.vcverifier.utils.DateUtils
 import io.mosip.vercred.vcverifier.utils.Util
 import org.json.JSONArray
 import org.junit.Assert.assertEquals
@@ -9,16 +10,17 @@ import org.junit.Test
 class UtilsTest {
 
     val utils = Util()
+    val dateUtils = DateUtils()
 
     @Test
     fun `test_validate_date_invalid`() {
-        val result = utils.isValidDate("123456789")
+        val result = dateUtils.isValidDate("123456789")
         assertEquals(false, result)
     }
 
     @Test
     fun `test_validate_date_valid`() {
-        val result = utils.isValidDate("2024-09-02T17:36:13.644Z")
+        val result = dateUtils.isValidDate("2024-09-02T17:36:13.644Z")
         assertEquals(true, result)
     }
 
@@ -60,19 +62,19 @@ class UtilsTest {
 
     @Test
     fun `date_expired`(){
-        val result = utils.isDateExpired("2024-09-02T17:36:13.644Z")
+        val result = dateUtils.isVCExpired("2024-09-02T17:36:13.644Z")
         assertEquals(true, result)
     }
 
     @Test
     fun `date_not_expired`(){
-        val result = utils.isDateExpired("2024-11-02T17:36:13.644Z")
+        val result = dateUtils.isDatePassedCurrentDate("2024-11-02T17:36:13.644Z")
         assertEquals(false, result)
     }
 
     @Test
     fun `invalid_date`(){
-        val result = utils.isDateExpired("12345")
+        val result = dateUtils.isDatePassedCurrentDate("12345")
         assertEquals(false, result)
     }
 }
